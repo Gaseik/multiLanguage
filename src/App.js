01110f3a8b071e9a6fbf31ee8055a1d6a5482f4f
James Chen <james.chen@cefinity.com>
2021-08-16 00:01:38 +0800

    inital

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import { FormattedMessage, FormattedDate,FormattedTime, IntlProvider } from "react-intl";
+import React, { useState, useEffect } from 'react'
+
 
 function App() {
+  const [lang, setLang] = useState(navigator.language)
+  const [locale, setLocale] = useState()
+  console.log(navigator.language)
+  useEffect(
+    () => {
+      const nn = async () => {
+        const resp = await fetch(`./lang/${lang}.json`)
+        const data = await resp.json()
+        setLocale(data)
+      }
+      nn()
+
+    }, [lang])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IntlProvider messages={locale}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            <select
+              value={lang}
+              onChange={(evt) => {
+                setLang(evt.target.value);
+              }}
+            >
+              <option value="en">English</option>
+              <option value="zh-TW">中文</option>
+              <option value="fr">Français</option>
+              <option value="jp">日本語</option>
+            </select>
+          </div>
+          <p>
+            <FormattedMessage
+              id="app.header"
+              defaultMessage="Edit src/App.js and save to reload."
+            />
+
+
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FormattedMessage
+              id="app.content"
+              defaultMessage="Learn React"
+            // defaultMessage={<div>Edit <code>src/App.js</code> and save to reload.</div>}
+            />
+
+          </a>
+          <FormattedDate
+            value={new Date()}
+            
+            year="numeric"
+            month="long"
+            day="numeric"
+            weekday="long"
+          />
+          <div>
+          <FormattedTime value={new Date()} />
+          </div>
+           
+        </header>
+      </div>
+    </IntlProvider>
   );
 }
 
